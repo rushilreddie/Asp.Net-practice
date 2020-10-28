934971a8b9c325dcf62a7f79db7894696e2be9c4
LAPTOP-1GR06HCI\rajar <rushil.r.e@gmail.com>
2020-10-28 17:09:11 -0500

    Practice

---
+++ ClassesAndObjects/ClassesAndObjects/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ClassesAndObjects
+{
+    public class Person
+    {
+        public string Name;
+   
+          public void Introduce(string to)
+        {
+            Console.WriteLine("Hi {0}, This is {1}", to, Name);
+        }
+       }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var person = new Person();
+            person.Name = "Rushil";
+            person.Introduce("Reddy");
+        }
+    }
+}
