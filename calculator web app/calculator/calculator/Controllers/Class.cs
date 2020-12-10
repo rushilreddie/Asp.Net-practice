7af8d405982a13b1bffcee3b7406cd999710a983
LAPTOP-1GR06HCI\rajar <rushil.r.e@gmail.com>
2020-12-09 22:05:37 -0600

    learning program.

---
+++ calculator web app/calculator/calculator/Controllers/Class.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+
+namespace calculator.Controllers
+{
+    public class Class
+    {
+        public int Add(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+        public int Subtract(int num1, int num2)
+        {
+            return num1 - num2;
+        }
+        public int Multiply(int num1, int num2)
+        {
+            return num1 * num2;
+        }
+        public double Divide(int num1, int num2)
+        {
+            return num1 / num2;
+        }
+        
+    }
+}
