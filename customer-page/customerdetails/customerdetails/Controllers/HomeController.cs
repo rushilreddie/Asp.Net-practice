9a2766ee066b3786c041daf2b3068fc53886911c
DESKTOP-AIO2UAN\rushilreddie <rushilreddy.js@gmail.com>
2020-12-14 20:20:56 -0600

    customer form page in a list

--- customer-page/customerdetails/customerdetails/Controllers/HomeController.cs
+++ customer-page/customerdetails/customerdetails/Controllers/HomeController.cs
@@ -12,13 +12,22 @@ namespace customerdetails.Controllers
     public class HomeController : Controller
     {
         private ILogger<HomeController> _logger;
+        private int CreateRandomNumber()
+        {
+            Random rnd = new Random();
+            int result= rnd.Next();
+            return result;
+        }
 
         public HomeController(ILogger<HomeController> logger)
         {
             _logger = logger;
         }
-
         public IActionResult Index()
+        {
+            return View("Createpage");
+        }
+        public IActionResult Createpage()
         {
             return View();
         }
@@ -39,13 +48,13 @@ namespace customerdetails.Controllers
 
             var mycustomerObject = new List<Customerform>()
             {
-               new Customerform { Fname = customerData.Fname },
-               new Customerform { Fname = "Sandeep",Lname = "CH",DateTime=DateTime.Now,Address="hgdfghf",Gender="m"},
-               new Customerform { Fname = "Rushil",Lname = "E",DateTime=DateTime.Now,Address="savasdfgasg",Gender="m"},
-               new Customerform { Fname = "Advaith",Lname = "CH",DateTime=DateTime.Now,Address="sdfbsdb",Gender="m"}
+               new Customerform { ID = CreateRandomNumber(), Fname = customerData.Fname, Lname=customerData.Lname, Address=customerData.Address, DateTime=customerData.DateTime, Gender=customerData.Gender},
+               new Customerform { ID = CreateRandomNumber(), Fname = "Sandeep",Lname = "CH",DateTime=DateTime.Now,Address="hgdfghf",Gender="m"},
+               new Customerform { ID = CreateRandomNumber(), Fname = "Rushil",Lname = "E",DateTime=DateTime.Now,Address="savasdfgasg",Gender="m"},
+               new Customerform { ID = CreateRandomNumber(), Fname = "Advaith",Lname = "CH",DateTime=DateTime.Now,Address="sdfbsdb",Gender="m"}
             };
 
-             return View("index", mycustomerObject);
+             return View("index", new CustomerViewModel { Customerforms=mycustomerObject});
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
